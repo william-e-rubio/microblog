4205966e0a98a2eae0067db0a9e0c91e4e5a082c
William <108055912+ItzOneWay@users.noreply.github.com>
2025-07-03 22:48:34 -0600

    Daily contributions

--- app/routes.py
+++ app/routes.py
@@ -4,5 +4,16 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():	
-	user = {'username': 'Miguel'}
-	return render_template('index.html', title='Home', user=user)
+	user = {'username': 'William Rubio'}
+	posts = [
+		{
+			'author' : {'username' : 'John'},
+			'body' : 'Beautiful day in Belize!'
+		},
+		{
+			'author' : {'username' : 'James'},
+			'body' : 'The day in the life of a 24 year old software engineer living in Belize!'
+			
+		}
+	]
+	return render_template('index.html', title='Home', user=user, posts=posts)
